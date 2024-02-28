9b0b39f9b6dbed5133eed8c8512c06d591447a58
Yeampier2022 <yeampierjesusa@gmail.com>
2024-02-28 15:32:11 -0400

    feat: errors handling

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 const baseController = require("./controllers/baseController")
 const process = require("process");
 const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities")
 
 app.set("view engine", "ejs")
 app.use(expressLayouts)
@@ -26,6 +27,27 @@ app.get("/", baseController.buildHome);
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
+
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  // console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
+
 
 /* ***********************
  * Local Server Information
