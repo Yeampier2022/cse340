7e86fc5f0fbc8cd0ac08010d103020ba05a85125
Yeampier2022 <yeampierjesusa@gmail.com>
2024-02-28 11:27:37 -0400

    feat: styles header

--- utilities/index.js
+++ utilities/index.js
@@ -8,7 +8,7 @@ Util.getNav = async function (req, res, next) {
 
   let data = await invModel.getClassifications()
 
-  let list = "<ul>"
+  let list = `<ul class="li-navigation">`
   list += '<li><a href="/" title="Home page">Home</a></li>'
   data.rows.forEach((row) => {
     list += "<li>"
