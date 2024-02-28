71e7de8add7427f450f7d1f22cf67c46551a2c63
Yeampier2022 <yeampierjesusa@gmail.com>
2024-02-28 12:15:51 -0400

    feat: styles invertory card

--- utilities/index.js
+++ utilities/index.js
@@ -9,7 +9,7 @@ Util.getNav = async function (req, res, next) {
   let data = await invModel.getClassifications()
 
   let list = `<ul class="li-navigation">`
-  list += '<li><a href="/" title="Home page">Home</a></li>'
+  list += '<li class="li-a"><a href="/" title="Home page">Home</a></li>'
   data.rows.forEach((row) => {
     list += "<li>"
     list +=
@@ -32,10 +32,10 @@ Util.buildClassificationGrid = async function(data){
   if(data.length > 0){
     grid = '<ul id="inv-display">'
     data.forEach(vehicle => { 
-      grid += '<li>'
+      grid += '<li class="card-info">'
       grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
       + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
-      + 'details"><img src="' + vehicle.inv_thumbnail 
+      + 'details"><img class="img-inventory" src="' + vehicle.inv_thumbnail 
       +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
       +' on CSE Motors" /></a>'
       grid += '<div class="namePrice">'
@@ -45,7 +45,7 @@ Util.buildClassificationGrid = async function(data){
       + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
       + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
       grid += '</h2>'
-      grid += '<span>$' 
+      grid += '<span class="car-price" >$' 
       + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
       grid += '</div>'
       grid += '</li>'
