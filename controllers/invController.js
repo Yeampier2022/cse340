582f62b93de371347e1158009235611eb9465bf4
Yeampier2022 <yeampierjesusa@gmail.com>
2024-03-06 13:29:12 -0400

    fix: error registration

--- controllers/invController.js
+++ controllers/invController.js
@@ -12,7 +12,7 @@ invCont.buildByClassificationId = async function (req, res, next) {
   const data = await invModel.getInventoryByClassificationId(classification_id)
   const grid = await utilities.buildClassificationGrid(data)
   let nav = await utilities.getNav()
-  const className = data[0].classification_name
+  const className = data[0]?.classification_name
   res.render("./inventory/classification", {
     title: className + " vehicles",
     nav,
