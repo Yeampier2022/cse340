f38b68df5c1f578103b7e8a43de8f83658f4c5ba
Yeampier2022 <yeampierjesusa@gmail.com>
2024-03-18 15:52:37 -0400

    feat: local form edit

--- public/js/inv-update.js
+++ public/js/inv-update.js
@@ -1,4 +1,4 @@
-const form = document.querySelector("#updateForm")
+const form = document.getElementById("updateForm")
     form.addEventListener("change", function () {
       const updateBtn = document.querySelector("button")
       updateBtn.removeAttribute("disabled")
