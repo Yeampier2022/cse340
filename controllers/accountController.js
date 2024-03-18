6a06108ba6030fc58fa10981961b79d2c36aae32
Yeampier2022 <yeampierjesusa@gmail.com>
2024-03-18 10:38:58 -0400

    fix: error hasdedPassword

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -26,7 +26,7 @@ async function registerAccount(req, res) {
 
 
 
-  let hasdhedPassword = await bcrypt.hash(account_password, 10)
+  let hasdhedPassword
 
   try {
     hasdhedPassword = await bcrypt.hash(account_password, 10)
@@ -43,7 +43,7 @@ async function registerAccount(req, res) {
     account_firstname,
     account_lastname,
     account_email,
-    account_password
+    hasdhedPassword
   )
 
   if (regResult) {
