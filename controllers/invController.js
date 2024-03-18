36893a274497ed948c50f68b379788c299d76ef2
Yeampier2022 <yeampierjesusa@gmail.com>
2024-03-18 15:01:18 -0400

    finish activities person

--- controllers/invController.js
+++ controllers/invController.js
@@ -52,30 +52,6 @@ invCont.getInventoryJSON = async (req, res, next) => {
   }
 }
 
-invCont.editInventoryView = async function (req, res, next) {
-  const inv_id = parseInt(req.params.inv_id)
-  let nav = await utilities.getNav()
-  const itemData = await invModel.getInventoryById(inv_id)
-  const classificationSelect = await utilities.buildClassificationList(itemData.classification_id)
-  const itemName = `${itemData.inv_make} ${itemData.inv_model}`
-  res.render("./inventory/edit-inventory", {
-    title: "Edit " + itemName,
-    nav,
-    classificationSelect: classificationSelect,
-    errors: null,
-    inv_id: itemData.inv_id,
-    inv_make: itemData.inv_make,
-    inv_model: itemData.inv_model,
-    inv_year: itemData.inv_year,
-    inv_description: itemData.inv_description,
-    inv_image: itemData.inv_image,
-    inv_thumbnail: itemData.inv_thumbnail,
-    inv_price: itemData.inv_price,
-    inv_miles: itemData.inv_miles,
-    inv_color: itemData.inv_color,
-    classification_id: itemData.classification_id
-  })
-}
 
 invCont.updateInventory = async function (req, res, next) {
   let nav = await utilities.getNav()
@@ -110,7 +86,7 @@ invCont.updateInventory = async function (req, res, next) {
     console.log("updateResult", updateResult);
     const itemName = updateResult[0].inv_make + " " + updateResult[0].inv_model
     req.flash("notice", `The ${itemName} was successfully updated.`)
-    res.redirect("inventory/management")
+    res.redirect("/inv")
   } else {
     const classificationSelect = await utilities.buildClassificationList(classification_id)
     const itemName = `${inv_make} ${inv_model}`
@@ -149,7 +125,7 @@ invCont.editInventoryView = async function (req, res, next) {
     nav,
     classificationSelect: classificationSelect,
     errors: null,
-    inv_id: itemData.inv_id,
+    inv_id: itemData[0].inv_id,
     inv_make: itemData.inv_make,
     inv_model: itemData.inv_model,
     inv_year: itemData.inv_year,
