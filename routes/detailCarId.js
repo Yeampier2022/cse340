8f6cc9ace9d13426d8875caed30dd49b0a48808e
Yeampier2022 <yeampierjesusa@gmail.com>
2024-03-26 15:17:44 -0400

    feat: logout detailCArd

--- routes/detailCarId.js
+++ routes/detailCarId.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = new express.Router();
 const invController = require('../controllers/invController');
 const utilities = require('../utilities/');
+const accountController = require('../controllers/accountController');
 
 
 router.get('/:carId', invController.buildDetailCarId);
 
+router.post("/logout", utilities.handleErrors(accountController.logout));
 
 module.exports = router;
