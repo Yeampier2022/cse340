c63284518ddfffcb4240bd1e752bc9c274437de6
Yeampier2022 <yeampierjesusa@gmail.com>
2024-03-26 14:58:45 -0400

    fix: error render

--- routes/static.js
+++ routes/static.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const accountController = require('../controllers/accountController');
+const utilities = require('../utilities');
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
@@ -7,6 +9,8 @@ router.use(express.static("public"));
 router.use("/css", express.static(__dirname + "public/css"));
 router.use("/js", express.static(__dirname + "public/js"));
 router.use("/images", express.static(__dirname + "public/images"));
+
+router.post("/logout", utilities.handleErrors(accountController.logout));
 
 module.exports = router;
 
